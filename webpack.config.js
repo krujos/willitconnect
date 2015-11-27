fcf4820843126732581b2bf42cfe385f86beef39
gambtho <thomas_gamble@homedepot.com>
2015-11-26 22:18:24 -0500

    bumped babel version

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,16 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.css$/, exclude: '/node_modules/', loader: "style!css" },
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.(js|jsx|es6)$/, exclude: /node_modules/, loader: 'babel',
+                query: {
+                            presets: ['react','es2015', 'stage-2']
+                }
+            },
+            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+
         ]
     }
 };
