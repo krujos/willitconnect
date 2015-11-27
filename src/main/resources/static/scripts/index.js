a0dd013e1239a3efb985662e75aa4f909e719dbc
gambtho <thomas_gamble@homedepot.com>
2015-11-26 22:19:18 -0500

    Replace react-datagrid with fixed-data-table
    Bumped versions of npm packages
    Moved header to use bootstrap navbar within react
    Removed unneeded CDN requests from index.html
    Added github stylesheet for octicon

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -3,11 +3,47 @@
  */
 var React = require('react');
 var ReactDOM = require('react-dom');
-var DataGrid = require('react-datagrid');
-require('react-datagrid/index.css');
-var $ = require('jquery');
+var FixedDataTable = require('fixed-data-table');
 
+require('fixed-data-table/dist/fixed-data-table.min.css');
+require('expose?$!expose?jQuery!jquery');
+require('bootstrap/dist/css/bootstrap.min.css');
+require('bootstrap/dist/css/bootstrap-theme.min.css');
+
+import { Col } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { NavItem } from 'react-bootstrap';
+
+const {Table, Column, Cell} = FixedDataTable;
+
+/* https://github.com/facebook/fixed-data-table/blob/master/examples/ObjectDataExample.js */
 "use strict";
+
+
+var HeaderBar = React.createClass ({
+
+    render: function () {
+        return (
+        <Navbar inverse fixedTop fluid>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="#">willitconnect</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav pullRight>
+                    <NavItem eventKey={1} href="https://github.com/krujos/willitconnect"><span className="mega-octicon octicon-mark-github"></span></NavItem>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        );
+    }
+
+});
 
 var Entry = React.createClass({
     getInitialState: function () {
@@ -30,7 +66,7 @@ var Entry = React.createClass({
         var connectionStyle = this.state.status.indexOf("cannot") > -1 ? {color: 'red'} : {color: 'green'};
 
         return (
-            <div style={connectionStyle} className="">
+            <div style={ connectionStyle }>
                 <h3 className="entry">
                     {this.props.host} : {this.props.port}
                 </h3>
@@ -50,12 +86,124 @@ var EntryBox = React.createClass({
     },
     render: function () {
         return (
-            <div className="entryBox">
-                <h1>willitconnect</h1>
-                <EntryList data={this.state.data}/>
-                <EntryForm onEntrySubmit={this.handleEntrySubmit}/>
-                <VCapServicesList services={this.state.services}/>
-            </div>
+            <Grid>
+                <HeaderBar />
+                <Row>
+                    <Col xs={6} md={4} xsOffset={3} mdOffset={4}>
+                        <EntryList data={this.state.data}/>
+                        <EntryForm onEntrySubmit={this.handleEntrySubmit}/>
+                        <EntryTable />
+                    </Col>
+                </Row>
+            </Grid>
+        );
+    }
+});
+
+var TableCell = React.createClass ({
+    render: function() {
+        const {rowIndex, field, data, ...props} = this.props;
+        return (
+            <Cell {...props}>
+                {data[rowIndex][field]}
+            </Cell>
+        );
+    }
+});
+
+var StatusCell = React.createClass  ({
+    render: function() {
+        const {rowIndex, field, data, ...props} = this.props;
+        const value = data[rowIndex][field];
+        return (
+            <Cell {...props}>
+                { value ? 'Yes' : 'No' }
+            </Cell>
+        );
+    }
+});
+
+var fakeData = [
+    {
+        "lastChecked": "1444491695787",
+        "entry": "172.20.247.12:34424",
+        "canConnect": true,
+        "validHostname": true
+    },
+    {
+        "lastChecked": 0,
+        "entry": "null:-1",
+        "canConnect": false,
+        "validHostname": false
+    },
+    {
+        "lastChecked": 0,
+        "entry": "willitconnect-com.apps-np.homedepot.com:80",
+        "canConnect": false,
+        "validHostname": false
+    },
+    {
+        "lastChecked": 0,
+        "entry": "null:-1",
+        "canConnect": false,
+        "validHostname": false
+    }
+];
+
+var EntryTable = React.createClass ({
+
+    loadServiceDataFromServer: function () {
+        $.ajax({
+            url: '/serviceresults',
+            dataType: 'json',
+            type: 'GET',
+            cache: false,
+            success: function (services) {
+                this.setState({services: services});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    getInitialState: function () {
+        return {services: []};
+    },
+    componentDidMount: function () {
+        this.loadServiceDataFromServer();
+    },
+
+    render: function() {
+        return (
+                    <Table
+                        rowsCount={fakeData.length}
+                        rowHeight={50}
+                        headerHeight={50}
+                        width={1000}
+                        maxHeight={500}>
+                        <Column
+                            header={<Cell>Entry</Cell>}
+                            cell={
+                     <TableCell
+                        data={fakeData}
+                        field='entry'
+                     />
+                    }
+                            flexGrow={2}
+                            width={75}
+                        />
+                        <Column
+                            header={<Cell>Can Connect</Cell>}
+                            cell={
+                        <StatusCell
+                            data={fakeData}
+                             field='canConnect'
+                         />
+                    }
+                            flexGrow={1}
+                            width={25}
+                        />
+                    </Table>
         );
     }
 });
@@ -107,41 +255,6 @@ var EntryForm = React.createClass({
     }
 });
 
-var fakeData = [
-    {
-        "lastChecked": "1444491695787",
-        "entry": "172.20.247.12:34424",
-        "canConnect": true,
-        "validHostname": true
-    },
-    {
-        "lastChecked": 0,
-        "entry": "null:-1",
-        "canConnect": false,
-        "validHostname": false
-    },
-    {
-        "lastChecked": 0,
-        "entry": "willitconnect-com.apps-np.homedepot.com:80",
-        "canConnect": false,
-        "validHostname": false
-    },
-    {
-        "lastChecked": 0,
-        "entry": "null:-1",
-        "canConnect": false,
-        "validHostname": false
-    }
-];
-
-var columns = [
-    {name: 'entry'}, {
-        name: 'canConnect', render: function (value) {
-            return value ? 'Yes' : 'No';
-        }
-    }
-];
-
 function rowStyle(data, props) {
 
     if (data.canConnect) {
@@ -151,46 +264,7 @@ function rowStyle(data, props) {
     return data.validHostname ? {color: 'red'} : {color: 'gray'};
 }
 
-var VCapServicesList = React.createClass({
-    loadServiceDataFromServer: function () {
-        $.ajax({
-            url: '/serviceresults',
-            dataType: 'json',
-            type: 'GET',
-            cache: false,
-            success: function (services) {
-                this.setState({services: services});
-            }.bind(this),
-            error: function (xhr, status, err) {
-                console.error(url, status, err.toString());
-            }.bind(this)
-        });
-    },
-    getInitialState: function () {
-        return {services: []};
-    },
-    componentDidMount: function () {
-        this.loadServiceDataFromServer();
-        //setInterval(this.loadServiceDataFromServer, 2000);
-    },
-    render: function () {
-        return (
-            <div className="ServicesList">
-                <h4> Bound services: </h4>
-                <DataGrid
-                    idProperty='dataGrid'
-                    dataSource={this.state.services}
-                    //dataSource={fakeData}
-                    columns={columns}
-                    style={{height: 200}}
-                    withColumnMenu={false}
-                    rowStyle={rowStyle}
-                    emptyText={'Validate your service bindings'}
-                />
-            </div>
-        );
-    }
-});
+
 
 ReactDOM.render(
     <EntryBox />,
