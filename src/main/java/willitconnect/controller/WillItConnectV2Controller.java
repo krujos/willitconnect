dce9f0a9565a244ed8df1c4b96895c7cf1d02c66
krujos <jkruck@pivotal.io>
2016-03-31 16:44:27 -0500

    add smoke test

--- src/main/java/willitconnect/controller/WillItConnectV2Controller.java
+++ src/main/java/willitconnect/controller/WillItConnectV2Controller.java
@@ -2,6 +2,7 @@ package willitconnect.controller;
 
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +16,13 @@ import static org.apache.log4j.Logger.getLogger;
 @RequestMapping(value="/v2")
 public class WillItConnectV2Controller {
 
+    EntryChecker entryChecker;
+
+    @Autowired
+    public WillItConnectV2Controller(EntryChecker entryChecker) {
+        this.entryChecker = entryChecker;
+    }
+
     private Logger log = getLogger(WillItConnectV2Controller.class);
     @RequestMapping(value="willitconnect")
     public @ResponseBody CheckedEntry willItConnect(
@@ -22,8 +30,7 @@ public class WillItConnectV2Controller {
 
         log.info(request.toString());
         JSONObject r = new JSONObject(request);
-        CheckedEntry entry = new CheckedEntry(r.getString("url"));
-        EntryChecker entryChecker = new EntryChecker();
+        CheckedEntry entry = new CheckedEntry(r.getString("target"));
         return entryChecker.check(entry);
     }
 }
--- src/test/java/willitconnect/controller/WillItConnectV2ControllerTest.java
+++ src/test/java/willitconnect/controller/WillItConnectV2ControllerTest.java
@@ -3,38 +3,49 @@ package willitconnect.controller;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import willitconnect.model.CheckedEntry;
+import willitconnect.service.EntryChecker;
 import willitconnect.service.util.Connection;
 
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.any;
+import static org.powermock.api.mockito.PowerMockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 public class WillItConnectV2ControllerTest {
     private MockMvc mockMvc;
 
-    static JSONObject REQUEST = new JSONObject().put("url", "https://pivotal.io");
+    static JSONObject REQUEST = new JSONObject().put("target", "https://pivotal.io");
+    @Mock
+    private EntryChecker entryChecker;
 
     @PrepareForTest(Connection.class)
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+
         mockMvc = MockMvcBuilders.standaloneSetup(
-                new WillItConnectV2Controller()).build();
+                new WillItConnectV2Controller(entryChecker)).build();
     }
 
     @Test
     public void itShouldConnectToAUrl() throws Exception {
+        CheckedEntry entry = new CheckedEntry(REQUEST.getString("target"));
+        entry.setCanConnect(true);
+
+        when(entryChecker.check(any(CheckedEntry.class))).thenReturn(entry);
         mockMvc.perform(get("/v2/willitconnect")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(REQUEST.toString()))
                 .andExpect(jsonPath("$.canConnect", is(true)))
-                .andExpect(jsonPath("$.entry", is(REQUEST.get("url"))));
+                .andExpect(jsonPath("$.entry", is(REQUEST.get("target"))));
     }
-
 }
