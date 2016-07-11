ebd485336978ee40d0e61b8ce6cf786e5cd44c03
Tom Gamble <thomas_gamble@homedepot.com>
2016-07-11 08:57:31 -0600

    fixed issue with multiple searches (#65)
    incorrect status show for subsequent searches
    updated to boot 1.3.6
    updated package.json
    continued conversion to es6 classes

--- src/main/scripts/EntryList.js
+++ src/main/scripts/EntryList.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import StatefulEntry from './Entry';
 
-var EntryList = React.createClass({
-    getInitialState: function () {
-        return {data: []};
-    },
-    render: function () {
+export default class EntryList extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {data: []};
+    }
+    render() {
+        console.table(this.props.data);
         var entryNodes = this.props.data.map(function (entry, index) {
             return (
                 <StatefulEntry host={entry.host} port={entry.port} status={entry.status} proxyHost={entry.proxyHost}
                        proxyPort={entry.proxyPort} key={index}/>
             );
-        });
+        }).reverse();
         return (
             <div className="entryList">
                 {entryNodes}
             </div>
         );
     }
-});
-
-module.exports = EntryList;
+};
--- src/main/scripts/test/EntryList.spec.jsx
+++ src/main/scripts/test/EntryList.spec.jsx
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-const EntryList = require('../EntryList');
+import EntryList from '../EntryList';
 import { shallow } from 'enzyme';
 
 describe('EntryList', () => {
@@ -16,7 +16,7 @@ describe('EntryList', () => {
 
     var entry0 = {"host": "google.com", "port": "80", "proxyHost": undefined, "proxyPort": undefined};
     var entry1 = {"host":"test.com", "port": "80", "status":"{canConnect:true}", "proxyHost":"", "proxyPort":""};
-    var entry2 = {"host":"test2.com", "port": "70", "status":"{canConnect:false}", "proxyHost":"proxy.com", "proxyPort":"60"}
+    var entry2 = {"host":"test2.com", "port": "70", "status":"{canConnect:false}", "proxyHost":"proxy.com", "proxyPort":"60"};
     var entries = [];
     entries.push(entry0);
     entries.push(entry1);
@@ -25,8 +25,9 @@ describe('EntryList', () => {
     it("displays the entryList", function() {
         const entryList = shallow(<EntryList data={entries} />);
         expect(entryList.find('StatefulEntry')).to.have.length.of(3);
-        expect(entryList.find('StatefulEntry').first().prop('host')).to.equal("google.com");
-        expect(entryList.find('StatefulEntry').at(2).prop('proxyPort')).to.equal('60');
+        expect(entryList.find('StatefulEntry').at(2).prop('host')).to.equal("google.com");
+        expect(entryList.find('StatefulEntry').at(1).prop('port')).to.equal('80');
+        expect(entryList.find('StatefulEntry').at(0).prop('status')).to.equal('{canConnect:false}');
     });
     
 });
