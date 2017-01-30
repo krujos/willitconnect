05e0e019462e222a83d04b037dfc86feae61be6c
Tom Gamble <tgamble@thoughtworks.com>
2017-01-30 17:23:05 -0500

    dependency bump, including webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
 
 module.exports = {
+      context: __dirname,
     entry: [
         'babel-polyfill',
         "./src/main/script/Index.js"
@@ -11,25 +14,44 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, exclude: '/node_modules/', loader: "style!css" },
-            { test: /\.(js|jsx|es6)$/, exclude: /node_modules/, loader: 'babel',
+            { test: /\.css$/, exclude: '/node_modules/', loader: 'style-loader!css-loader' },
+            { test: /\.(js|jsx|es6)$/, exclude: /node_modules/, loader: 'babel-loader',
                 query: {
                             plugins: ['transform-runtime'],
                             presets: ['react','es2015', 'stage-0']
                 }
             },
-            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'}
 
         ]
     },
     resolve: {
-        moduleDirectories: [
+        modules: [
             path.join(__dirname, `node_modules`),
             path.join("./src/main/script/Index.js")
         ],
-        extensions: [``, `.js`]
-    }
+        extensions: [`.js`, `.jsx`]
+    },
+  plugins: debug ? [] : [
+      new webpack.optimize.UglifyJsPlugin({
+        mangle: false,
+        sourcemap: false,
+        compress: {
+          warnings: false,
+          screw_ie8: true,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true,
+        }
+      })
+    ],
 };
+
