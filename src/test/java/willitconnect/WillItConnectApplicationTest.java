de0dc723ee5802ef13631ba37e280911afacc610
Tom Gamble <thomasgamble2@gmail.com>
2017-02-27 13:46:15 -0500

    dependency bump, including webpack2 (#111)
    * dependency bump, including webpack2
    
    * simplified wercker script - moved back to java box
    * updated spring boot, gradle, and all npm dependencies

--- src/test/java/willitconnect/WillItConnectApplicationTest.java
+++ src/test/java/willitconnect/WillItConnectApplicationTest.java
@@ -1,15 +1,12 @@
 package willitconnect;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = WillItConnectApplication.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class WillItConnectApplicationTest {
 
     @Test
