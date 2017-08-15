aa7a511c28bccad26982f566c6d15ccdf307a85e
gambtho <tgamble@thougthworks.com>
2017-08-15 14:26:11 -0400

    updating (almost) all the things

--- src/test/script/EntryList.spec.jsx
+++ src/test/script/EntryList.spec.jsx
@@ -6,7 +6,7 @@ import { shallow } from 'enzyme';
 
 describe('EntryList', () => {
     
-    var mixpanel = {
+    const mixpanel = {
         track: function () {}
     };
 
@@ -14,17 +14,17 @@ describe('EntryList', () => {
         window.mixpanel = mixpanel;
     });
 
-    var entry0 = {"host": "google.com", "port": "80", "proxyHost": undefined, "proxyPort": undefined};
-    var entry1 = {"host":"test.com", "port": "80", "status":"{canConnect:true}", "proxyHost":"", "proxyPort":""};
-    var entry2 = {"host":"test2.com", "port": "70", "status":"{canConnect:false}", "proxyHost":"proxy.com", "proxyPort":"60"};
-    var entries = [];
+    const entry0 = {"host": "google.com", "port": "80", "proxyHost": undefined, "proxyPort": undefined};
+    const entry1 = {"host": "test.com", "port": "80", "status": "{canConnect:true}", "proxyHost": "", "proxyPort": ""};
+    const entry2 = {"host":"test2.com", "port": "70", "status":"{canConnect:false}", "proxyHost":"proxy.com", "proxyPort":"60"};
+    const entries = [];
     entries.push(entry0);
     entries.push(entry1);
     entries.push(entry2);
 
     it("displays the entryList", function() {
         const entryList = shallow(<EntryList data={entries} />);
-        expect(entryList.find('StatelessEntry')).to.have.length.of(3);
+        expect(entryList.find('StatelessEntry').length).to.eq(3);
         expect(entryList.find('StatelessEntry').at(2).prop('host')).to.equal("google.com");
         expect(entryList.find('StatelessEntry').at(1).prop('port')).to.equal('80');
         expect(entryList.find('StatelessEntry').at(0).prop('status')).to.equal('{canConnect:false}');
